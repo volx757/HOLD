1ec5a111a105417167f8213e5978de621d38c6ff
volx757 <bendbarclay@gmail.com>
2015-09-09 03:36:49 -0400

    qa

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,7 +11,10 @@ $(document).ready(function () {
     $('#huge-logo').removeClass('initial-pos')
     logoGame()
     setTimeout(function () {
-        $('#containment, #social').removeClass('initial-pos')
+        $('#social').removeClass('initial-pos')
+        setTimeout(function(){
+            $('#containment').removeClass('initial-pos')
+        }, 150)
 
         $('.fa:eq(0)').addClass('glow').removeClass('initial-pos')
 
@@ -25,24 +28,24 @@ $(document).ready(function () {
             setTimeout(function () {
                 setTimeout(function () {
                     $('.fa:eq(1)').removeClass('glow')
-                }, 200)
+                }, 100)
                 $('.fa:eq(2)').addClass('glow').removeClass('initial-pos')
 
                 setTimeout(function () {
                     setTimeout(function () {
                         $('.fa:eq(2)').removeClass('glow')
-                    }, 200)
+                    }, 100)
                     $('.fa:eq(3)').addClass('glow').removeClass('initial-pos')
 
                     setTimeout(function () {
                         setTimeout(function () {
                             $('.fa:eq(3)').removeClass('glow')
-                        }, 100)
+                        }, 50)
                         $('.fa:eq(4)').addClass('glow').removeClass('initial-pos')
 
                         setTimeout(function () {
                             $('.fa:eq(4)').removeClass('glow')
-                        }, 250)
+                        }, 70)
 
                     }, 100)
                 }, 100)
@@ -60,15 +63,14 @@ $(document).ready(function () {
         $('body').addClass('no-cursor')
         $(this).css('transform', 'translate(0,25px)').css('opacity', '0.5').addClass('no-point')
         $('#gif').css('z-index', '1')
-        $('#fadecover').removeClass('off')
+        $('#fadecover').removeClass('transition-slow').addClass('transition-slow-out').removeClass('off')
         setTimeout(function () {
             $('#containment, #social').addClass('initial-pos')
-
-        }, 2500)
+        }, 400)
         setTimeout(function () {
             window.location = '/'
 
-        }, 3400)
+        }, 1200)
     })
 })
 
